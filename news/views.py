58a045e0e6411cf35a7eb7d789908054f565fed7
VaynZXC <venya_productions@mail.ru>
2023-07-25 23:10:40 +0300

    'news'

--- news/views.py
+++ news/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from news.models import Post
+from django.utils import timezone
 
 # Create your views here.
 class PostsList(ListView):
@@ -8,6 +9,12 @@ class PostsList(ListView):
     template_name = 'news/allnews.html'
     context_object_name = 'allNews'
     queryset = Post.objects.order_by('-id')
+
+    def get_context_data(self, **kwargs):
+      context = super().get_context_data(**kwargs)
+      context['time_now'] = timezone.localtime(timezone.now())
+      context['value1'] = None
+      return context
 
 class PostDetail(DetailView):
     model = Post
